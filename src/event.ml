116bc7118c6370e28dd087ff4e1ae2ee382f4172
Brendan Long <self@brendanlong.com>
2018-05-31 20:42:20 -0400

    Include stack traces

--- src/event.ml
+++ src/event.ml
@@ -13,12 +13,32 @@ type mechanism = Payloads_t.mechanism =
   (* TODO: meta *)
   ; data : (string * string) list option }
 
+type stackframe = Payloads_t.stackframe =
+  { filename : string option
+  ; function_ : string option
+  ; module_ : string option
+  ; lineno : int option
+  ; colno : int option
+  ; abs_path : string option
+  ; context_line : string option
+  ; pre_context : string list option
+  ; post_context : string list option
+  ; in_app : bool option
+  ; vars : (string * string) list option
+  ; package : string option
+  ; platform : Platform.t option
+  (* TODO: image_addr, instruction_addr, symbol_addr, instruction_offset *) }
+
+type stacktrace = Payloads_t.stacktrace =
+  { frames : stackframe list }
+
 type exception_value = Payloads_t.exception_value =
   { type_ : string
   ; value : string
   ; module_ : string option
   ; thread_id : string option
-  ; mechanism : mechanism option }
+  ; mechanism : mechanism option
+  ; stacktrace : stacktrace option }
 
 type exception_ = Payloads_t.exception_ =
   { values : exception_value list }
@@ -71,11 +91,39 @@ let make ?event_id ?timestamp ?(logger="ocaml") ?(platform=`Other)
   let exception_ =
     match exn with
     | Some exn ->
+      let frames =
+        Caml.Printexc.get_raw_backtrace ()
+        |> Caml.Printexc.backtrace_slots
+        |> Option.map ~f:(fun slots ->
+          Array.to_list slots
+          |> List.filter_map ~f:(fun frame ->
+            match Caml.Printexc.Slot.location frame with
+            | None -> None
+            | Some { Caml.Printexc.filename ; line_number ; start_char ; end_char } ->
+              Some { filename = Some filename
+                   ; function_ = None
+                   ; module_ = None
+                   ; lineno = Some line_number
+                   ; colno = Some start_char
+                   ; abs_path = None
+                   ; context_line = None
+                   ; pre_context = None
+                   ; post_context = None
+                   ; in_app = None
+                   ; vars = None
+                   ; package = None
+                   ; platform = None }))
+        |> function
+        | Some [] -> None
+        | l -> l
+      in
       Some { values = [{ type_ = Caml.Printexc.exn_slot_name exn
                        ; value = Caml.Printexc.to_string exn 
                        ; module_ = None
                        ; thread_id = None
-                       ; mechanism = None }] }
+                       ; mechanism = None
+                       ; stacktrace = Option.map frames ~f:(fun frames ->
+                           { frames }) }] }
     | None -> None
   in
   { event_id ; timestamp ; logger ; platform ; sdk ; level ; culprit
