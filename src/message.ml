37c9bb2f8488ba96cb75349eca8d41db3da9507c
Brendan Long <self@brendanlong.com>
2018-08-03 07:36:45 -0400

    Add message tests

--- src/message.ml
+++ src/message.ml
@@ -12,3 +12,18 @@ let make ~message ?(params=[]) ?formatted () =
 
 let to_payload { message ; params ; formatted } =
   { Payloads_t.message ; params = empty_list_option params ; formatted }
+
+let%expect_test "to_payload with params" =
+  make ~message:"My raw message with interpolated strings like %s"
+    ~params:["this" ; "is an example"] ()
+  |> to_payload
+  |> Payloads_j.string_of_message
+  |> print_endline;
+  [%expect {| {"message":"My raw message with interpolated strings like %s","params":["this","is an example"]} |}]
+
+let%expect_test "to_payload without params" =
+  make ~message:"Lorem ipsum" ()
+  |> to_payload
+  |> Payloads_j.string_of_message
+  |> print_endline;
+  [%expect {| {"message":"Lorem ipsum"} |}]
